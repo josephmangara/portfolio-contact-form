87c61863c7aa424deec868c33a6d23155f0bfb2c
josephmangara <josephmangara2@gmail.com>
2024-05-29 20:26:10 +0300

    connecting to render db

--- server/app.py
+++ server/app.py
@@ -12,9 +12,9 @@ import os
 
 load_dotenv()
 
-app = Flask(__name__)
+app = Flask(__name__,)
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///clients.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = True
 
