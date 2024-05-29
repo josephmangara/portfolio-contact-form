3ab8b3d10b452021f10a566c0f341e36362b0f3a
josephmangara <josephmangara2@gmail.com>
2024-05-29 20:48:06 +0300

    app

--- server/app.py
+++ server/app.py
@@ -14,6 +14,7 @@ load_dotenv()
 
 app = Flask(__name__,)
 CORS(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = True
